f47ea3abf7299db97a8b6946feea0d3f501b9408
coriolan <marcosloic@gmail.com>
2015-11-04 11:50:05 +0100

    Added bodyParser

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 
 var express = require('express'),
-    http = require('http');
+    http = require('http'),
+    bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 8888;
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 
 // Dossier public pour servir des assets statiques
 app.use("/", express.static(__dirname + "/src/public/"));
+
+app.use('/*', function(req, res){
+  res.sendFile(__dirname + '/src/public/index.html');
+});
+
 
 // 3…2…1…Démarrage
 var server = http.createServer(app).listen(port, function() {
